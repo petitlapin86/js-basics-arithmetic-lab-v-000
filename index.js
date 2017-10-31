cf0c598e6932ab321f68f64a2144e8c549eb2a72
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-31 19:37:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 const newID = 1000000000 + oldID;
-
+/* this is converting an old method to a new method (changing its number) */
 const ageIsValid = Number.isInteger(currentAge);
-
+/* this is an example of require true to the number being a valid integer */
 const randomNumber = Math.random() * 20;
-
+/* this is a method to pick a random number from 0-20 */
 const randomInteger = Math.floor(randomNumber);
-
+/* this is a method to take the value of randomnumber and round it down to the nearest integer */
 
 const randomUserID = randomInteger + 1000000001;
+/* */
